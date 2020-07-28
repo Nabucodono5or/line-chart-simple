5867d420423f678af483865ab363d0ffa18f3bd7
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-07-28 08:00:46 -0300

    inserido a fonte de dados e a criação de um scatterplot

--- index.js
+++ index.js
@@ -1,3 +1,73 @@
-import { select, selectAll } from "d3-select";
+import { select, selectAll } from "d3-selection";
+import { csv } from "d3-fetch";
+import { scaleLinear } from "d3-scale";
+import { axisBottom, axisLeft } from "d3-axis";
 
-select("svg").append("h1").html("Hello World");
+// select("body").append("h1").html("Hello World");
+
+csv(require("./data/tweetData.csv")).then((data) => {
+  console.log(data);
+  lineChart(data);
+});
+
+function lineChart(incomingData) {
+  let xScale = scaleLinear().domain([1, 11]).range([20, 470]);
+  let ySCale = scaleLinear().domain([0, 35]).range([470, 20]);
+
+  let xAxis = axisBottom(xScale).tickSize(-490).tickPadding(10);
+  let yAxis = axisLeft(ySCale).tickSize(-470).tickPadding(10);
+
+  select("svg")
+    .append("g")
+    .attr("class", "xAxis")
+    .attr("transform", "translate(0, 480)")
+    .call(xAxis);
+
+  select("svg")
+    .append("g")
+    .attr("class", "yAxis")
+    .attr("transform", "translate(20, 10)")
+    .call(yAxis);
+
+  select("svg")
+    .selectAll("circle.tweets")
+    .data(incomingData)
+    .enter()
+    .append("circle")
+    .attr("class", "tweets")
+    .attr("r", 5)
+    .attr("cx", (d) => {
+      return xScale(d.day);
+    })
+    .attr("cy", (d) => {
+      return ySCale(d.tweets);
+    });
+
+  select("svg")
+    .selectAll("circle.retweets")
+    .data(incomingData)
+    .enter()
+    .append("circle")
+    .attr("class", "retweets")
+    .attr("r", 5)
+    .attr("cx", (d) => {
+      return xScale(d.day);
+    })
+    .attr("cy", (d) => {
+      return ySCale(d.retweets);
+    });
+
+  select("svg")
+    .selectAll("circle.favorites")
+    .data(incomingData)
+    .enter()
+    .append("circle")
+    .attr("class", "favorites")
+    .attr("r", 5)
+    .attr("cx", (d) => {
+      return xScale(d.day);
+    })
+    .attr("cy", (d) => {
+      return ySCale(d.favorites);
+    });
+}
